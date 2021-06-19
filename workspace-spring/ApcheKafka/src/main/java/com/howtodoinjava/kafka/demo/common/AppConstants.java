a1c507b008bc23897c3da3743181b4e47b95c3e2
ashish34630 <80678140+ashish34630@users.noreply.github.com>
2021-06-20 03:18:27 +0530

    No use

--- workspace-spring/ApcheKafka/src/main/java/com/howtodoinjava/kafka/demo/common/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.howtodoinjava.kafka.demo.common;
-
-public class AppConstants {
-	
-	public static final String TOPIC_NAME = "test";
-    public static final String GROUP_ID = "group_id";
-
-}
