0050dad1c1b11144bf28ed904d8cc036b5891616
ashish34630 <80678140+ashish34630@users.noreply.github.com>
2021-06-20 03:46:22 +0530

    Delete workspace-spring/Today10 directory

--- workspace-spring/Today10/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Alien VALUES (12,'assd');
-INSERT INTO Alien VALUES (15,'assd');
-INSERT INTO Alien VALUES (17,'assd');
-INSERT INTO Alien VALUES (18,'assd');
-INSERT INTO Alien VALUES (19,'assd');
